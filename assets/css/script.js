4018e63520a732740ca9cf17f18a29c2aef576d6
CharlesBrant <charlefuture@gmail.com>
2022-11-17 13:50:24 -0300

    finalizaçao de pagina nascente

--- assets/css/script.js
+++ assets/css/script.js
@@ -47,7 +47,7 @@ $(document).ready(function () {
 
         }
     });
-
+    //cadasto no site
     $("#cadastrar").click(function () {
         var nome = $("[placeholder='Nome']").val();
         var dia = $("[placeholder='Tag']").val();
@@ -67,5 +67,25 @@ $(document).ready(function () {
         $("input").val("");
         editor.setData('');
 
+    });
+    //nascente
+    $("#cadastro").click(function () {       
+        var nome2 = $("#Nome").val();
+        var tag2 = $("#Tag").val();
+        var game2 = $("#Game").val();
+        var hora2 = $("#Hora").val();
+
+        var html2 = "<tr>";
+        html2 += "<td></td>";
+        html2 += "<td>" + nome2 + "</td>";
+        html2 += "<td>" + tag2 + "</td>";
+        html2 += "<td>" + game2 + "</Ttd>";
+        html2 += "<td>" + hora2 + "</td>"
+        html2 += "<td><a onclick='excluir(this)' href='#'><i class='fa fa-trash'></a></td>"
+        html2 += "</tr>"
+
+        $("#lista").append(html2)
+        $("input").val("");
+        
     });
 });
